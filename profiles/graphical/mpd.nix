173a6faf88b0a6aa36690d911c247c4b15b69fb9
foldu <foldu@protonmail.com>
2021-03-17 15:07:09 +0100

    Enable ncmpcpp visualizer

--- profiles/graphical/mpd.nix
+++ profiles/graphical/mpd.nix
@@ -8,13 +8,21 @@
         type "pulse"
         name "mpd"
       }
+      audio_output {
+        type                    "fifo"
+        name                    "mpd_fifo"
+        path                    "/run/user/1000/mpd.fifo"
+        format                  "44100:16:2"
+      }
     '';
   };
 
   services.mpdris2.enable = true;
 
   home.packages = [
-    pkgs.ncmpcpp
+    (pkgs.ncmpcpp.override {
+      visualizerSupport = true;
+    })
   ];
 
   xdg.configFile."ncmpcpp" = {
