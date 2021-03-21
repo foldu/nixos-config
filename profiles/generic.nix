4f7a639c7f46534ccde3b33c3fefb366a92f345f
foldu <foldu@protonmail.com>
2021-03-21 06:58:16 +0100

    Add myself to NetworkManager group

--- profiles/generic.nix
+++ profiles/generic.nix
@@ -52,7 +52,7 @@
     isNormalUser = true;
     uid = 1000;
     shell = pkgs.fish;
-    extraGroups = [ "wheel" "video" "audio" "dialout" ];
+    extraGroups = [ "wheel" "video" "audio" "dialout" "networkmanager" ];
   };
 
   # why is this false by default?
