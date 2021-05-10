c7f3d28f83448aad36b60e80c8007ffd1f46d982
foldu <foldu@protonmail.com>
2021-05-10 21:33:33 +0200

    Add gtk4 settings

--- profiles/graphical/gtk.nix
+++ profiles/graphical/gtk.nix
@@ -1,14 +1,27 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }: 
+let
+  iconTheme = "Pop";
+  gtkTheme = "Pop-dark";
+in
+{
   gtk = {
     enable = true;
     iconTheme = {
       package = pkgs.pop-icon-theme;
-      name = "Pop";
+      name = iconTheme;
     };
     theme = {
       package = pkgs.pop-gtk-theme;
-      name = "Pop-dark";
+      name = gtkTheme;
     };
     gtk3.extraConfig.gtk-key-theme-name = "Emacs";
   };
+
+  xdg.configFile."gtk-4.0/settings.ini".text = ''
+    [Settings]
+    gtk-application-prefer-dark-theme=1
+    gtk-font-name=Inter 11
+    gtk-icon-theme-name=${iconTheme}
+    gtk-theme-name=${gtkTheme}
+  '';
 }
