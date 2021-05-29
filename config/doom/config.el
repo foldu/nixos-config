0aaebe2327d4ea0fdd1321675f0697a04517a6dc
foldu <foldu@protonmail.com>
2021-05-29 12:41:38 +0200

    emacs: Add rename function

--- config/doom/config.el
+++ config/doom/config.el
@@ -206,6 +206,20 @@
 ;; reduce avy timeout waiting
 (setq avy-timeout-seconds 0.2)
 
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
