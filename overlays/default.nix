b9d552645a2058bbd503f4c076420b3a1e66b64d
foldu <foldu@protonmail.com>
2021-06-01 12:19:55 +0200

    gnome: Remove already existing extension packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,5 @@
 final: prev: {
   gnomeExtensions = prev.gnomeExtensions // {
-    blur-my-shell = prev.callPackage ../packages/blur-my-shell {};
-    just-perfection = prev.callPackage ../packages/just-perfection {};
     pop-os-shell = prev.callPackage ../packages/pop-os-shell {};
     gnome-ui-tune = prev.callPackage ../packages/gnome-ui-tune {};
   };
