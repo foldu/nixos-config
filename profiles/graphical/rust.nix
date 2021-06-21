78108a8188acfc765d408b755c962843a60247a1
foldu <foldu@protonmail.com>
2021-06-21 11:58:46 +0200

    Use rust stable

--- profiles/graphical/rust.nix
+++ profiles/graphical/rust.nix
@@ -1,20 +1,22 @@
 { config, lib, pkgs, ... }: {
 
   home.packages = with pkgs; [
-    (
-      pkgs.rust-bin.nightly.latest.minimal.override {
-        extensions = [
-          "rust-src"
-          "clippy"
-        ];
-        targets = [
-          "x86_64-unknown-linux-musl"
-          "aarch64-unknown-linux-gnu"
-          "wasm32-unknown-unknown"
-          "wasm32-wasi"
-        ];
-      }
-    )
+    #(
+    #  pkgs.rust-bin.nightly.latest.minimal.override {
+    #    extensions = [
+    #      "rust-src"
+    #      "clippy"
+    #    ];
+    #    targets = [
+    #      "x86_64-unknown-linux-musl"
+    #      "aarch64-unknown-linux-gnu"
+    #      "wasm32-unknown-unknown"
+    #      "wasm32-wasi"
+    #    ];
+    #  }
+    #)
+    rustc
+    cargo
     rust-analyzer
     rustfmt
     cargo-deny
