95cfc525d8d456bec75ed8949ac64ef4f0f8dc46
foldu <foldu@protonmail.com>
2021-08-17 11:45:32 +0200

    Enable debugging for bluez

--- profiles/bluetooth.nix
+++ profiles/bluetooth.nix
@@ -1,9 +1,12 @@
 { config, lib, pkgs, ... }: {
   hardware.bluetooth = {
     enable = true;
-    package = pkgs.bluezFull;
+    package = pkgs.enableDebugging pkgs.bluezFull;
     powerOnBoot = false;
   };
 
   systemd.services.bluetooth-mesh.aliases = [ "dbus-org.bluez.mesh.service" ];
+  systemd.services.bluetooth.serviceConfig.ExecStart = [ "" "${pkgs.bluezFull}/libexec/bluetooth/bluetoothd -f /etc/bluetooth/main.conf -d" ];
+  systemd.services.bluetooth-mesh.serviceConfig.ExecStart = [ "" "${pkgs.bluezFull}/libexec/bluetooth/bluetooth-meshd -d" ];
 }
+
