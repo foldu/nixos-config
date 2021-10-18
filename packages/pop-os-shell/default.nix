eb8ebf21979b6b6b61df339cfa7e1fa54ccbb470
foldu <foldu@protonmail.com>
2021-10-18 13:39:16 +0200

    fix(pop-os-shell): Get it to start up on GNOME 41
    XWayland Windows seem to have some problems but they after turning
    them off and on again

--- packages/pop-os-shell/default.nix
+++ packages/pop-os-shell/default.nix
@@ -2,28 +2,24 @@
 
 stdenv.mkDerivation rec {
   pname = "pop-os-shell";
-  version = "9507dc38f75f56e657cf071d5f8dc578c5dc9352";
+  version = "e7f30249a18317de3a54dc185e3a547ee4a74022";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "shell";
     rev = version;
-    sha256 = "sha256-9Z1SDg9TWdk5W22FkGYGQW1iMdS+T+hUpzZMW7whKZg=";
+    sha256 = "sha256-TULRwO+23V0/9Du8nolWfjWz9TLg2V3COneZVB+DLX0=";
   };
 
   nativeBuildInputs = [ glib nodePackages.typescript gjs ];
 
   buildInputs = [ gjs ];
 
-  patches = [
-    ./fix-gjs.patch
-    (fetchpatch {
-      url = "https://github.com/pop-os/shell/commit/1e24cd7ed64b138f1aa8b2aceb31ce9f55eaf594.patch";
-      sha256 =
-        "sha256-V18/7K45QAeVM4SkchHKUvvHww8oaet/tB5ReoU0Vr8=";
+  preBuildHook = ''
+    find . -name '*.ts' -exec sed -i -E 's|^#!/usr/bin/gjs|#!/usr/bin/env gjs|' \{\} \;
+  '';
 
-    })
-  ];
+  patches = [ ./gnome-41.patch ];
 
   makeFlags = [ "INSTALLBASE=$(out)/share/gnome-shell/extensions PLUGIN_BASE=$(out)/share/pop-shell/launcher SCRIPTS_BASE=$(out)/bin" ];
 
