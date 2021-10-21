2d07c9e99e2baba006a7b4f84096d8e8d056ef96
foldu <foldu@protonmail.com>
2021-10-21 11:46:00 +0200

    fix(qt): Force wayland

--- profiles/graphical/qt5.nix
+++ profiles/graphical/qt5.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
   #programs.qt5ct.enable = true;
+  environment.sessionVariables."QT_QPA_PLATFORM" = "wayland";
   environment.systemPackages = [
     pkgs.breeze-gtk
     pkgs.breeze-icons
