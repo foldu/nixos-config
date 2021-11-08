8bba55161194183640a8913091f81e6e75a44be9
foldu <foldu@protonmail.com>
2021-11-08 18:37:46 +0100

    fix(transmission): Update host whitelist to use nebula

--- profiles/homeserver/transmission.nix
+++ profiles/homeserver/transmission.nix
@@ -5,7 +5,7 @@
       download-dir = "/srv/media/aux/downloads";
       incomplete-dir = "/srv/media/aux/downloads/.incomplete";
       incomplete-dir-enabled = true;
-      rpc-host-whitelist = "torrent.5kw.li";
+      rpc-host-whitelist = "torrent.nebula.5kw.li";
       rpc-whitelist = "127.0.0.1,192.168.1.*,192.168.100.*";
     };
     port = 9091;
