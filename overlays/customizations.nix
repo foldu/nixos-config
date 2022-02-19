a3d178d721589253ce6ff03cc5ff5a1fed8e8e0d
foldu <foldu@protonmail.com>
2022-02-19 10:38:01 +0100

    chore: Update Brave in overlay
    Fixes some important Chromium security issue

--- overlays/customizations.nix
+++ overlays/customizations.nix
@@ -18,23 +18,29 @@ final: prev: {
       }
     );
   };
-  brave = prev.brave.overrideAttrs (oldAttrs: {
-    postFixup =
-      let
-        libPath = prev.lib.makeLibraryPath [ prev.libglvnd prev.wayland ];
-        flags = [
-          "--enable-features=UseOzonePlatform"
-          "--ozone-platform=wayland"
-          "--ignore-gpu-blocklist"
-          "--enable-gpu-rasterization"
-          "--enable-zero-copy"
-        ];
-        flagString = prev.lib.concatStringsSep " " flags;
-      in
-      # FIXME: this is a terrible abomination
-      ''
-        sed -i -E 's/^exec.+/\0 ${flagString}/' "$out/bin/brave"
-        sed -i -E 's|^exec.+|export LD_LIBRARY_PATH=${libPath}\n\0|' "$out/bin/brave"
-      '';
-  });
+  brave = prev.brave.overrideAttrs (oldAttrs:
+    let version = "1.35.103"; in
+    {
+      src = prev.fetchurl {
+        url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
+        sha256 = "UgperKruN2quKdFTf/iTa+dd2GB57nt+mu6KBe4VvYk=";
+      };
+      postFixup =
+        let
+          libPath = prev.lib.makeLibraryPath [ prev.libglvnd prev.wayland ];
+          flags = [
+            "--enable-features=UseOzonePlatform"
+            "--ozone-platform=wayland"
+            "--ignore-gpu-blocklist"
+            "--enable-gpu-rasterization"
+            "--enable-zero-copy"
+          ];
+          flagString = prev.lib.concatStringsSep " " flags;
+        in
+        # FIXME: this is a terrible abomination
+        ''
+          sed -i -E 's/^exec.+/\0 ${flagString}/' "$out/bin/brave"
+          sed -i -E 's|^exec.+|export LD_LIBRARY_PATH=${libPath}\n\0|' "$out/bin/brave"
+        '';
+    });
 }
