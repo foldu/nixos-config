b42f7fa1734aaf96a8a3c77284b940a5eeec3835
foldu <foldu@protonmail.com>
2022-03-31 13:51:11 +0200

    feat(rust): Automatic rustup update

--- profiles/graphical/rust.nix
+++ profiles/graphical/rust.nix
@@ -9,6 +9,20 @@
     cargo-bloat
   ];
 
+  systemd.user = {
+    services.rustup-update = {
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.rustup}/bin/rustup update";
+      };
+    };
+    timers.rustup-update = {
+      Timer = {
+        OnCalendar = "weekly";
+      };
+    };
+  };
+
   # use a linker that doesn't suck
   home.file.".cargo/config".text = ''
     [target.x86_64-unknown-linux-gnu]
