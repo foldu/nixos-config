c89ef52c018604cff569ba40afff2ef63d27c531
foldu <foldu@protonmail.com>
2022-07-18 12:13:47 +0200

    chore: Refactor netmaker-resolve into script

--- profiles/home/dns.nix
+++ profiles/home/dns.nix
@@ -11,27 +11,7 @@
     partOf = [ "netclient.service" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig.Type = "simple";
-    # FIXME: bad
-    script = ''
-      setup_resolve() {
-          echo "Setting resolvers for nm-home"
-          resolvectl domain nm-home '~home.5kw.li'
-          resolvectl dns nm-home 10.20.30.3
-      }
-
-      setup_resolve
-
-      exists=1
-      ${pkgs.iproute2}/bin/ip monitor link |
-          while read -r line; do
-              if echo "$line" | grep -Eq '^Deleted.+nm-home'; then
-                  exists=0
-              elif test "$exists" -eq 0 && echo "$line" | grep -q 'nm-home' && ! echo "$line" | grep -q DOWN; then
-                  exists=1
-                  echo "Change detected"
-                  setup_resolve
-              fi
-          done
-    '';
+    path = [ pkgs.iproute2 pkgs.bash ];
+    script = "${./netmaker-resolve.sh}";
   };
 }
