33408a6e32ac2ccb06f7bc461431bef5e6b7d622
foldu <foldu@protonmail.com>
2022-09-30 17:46:07 +0200

    fix: Add all needed locales

--- profiles/generic.nix
+++ profiles/generic.nix
@@ -10,7 +10,7 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ "de_DE.UTF-8/UTF-8" ];
+    supportedLocales = [ "de_DE.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
   };
 
   time.timeZone = "Europe/Amsterdam";
