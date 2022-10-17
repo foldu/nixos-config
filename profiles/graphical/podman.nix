4ffa455ce29d4fdb91892d6fabec0be93ddfa533
foldu <foldu@protonmail.com>
2022-10-17 20:44:17 +0200

    fix: docker download timeouts

--- profiles/graphical/podman.nix
+++ profiles/graphical/podman.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }: {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    # this first world shithole does not have internet
+    # and concurrent downloads cause docker to timeout
+    extraOptions = "--max-concurrent-downloads 1";
+    enable = true;
+  };
   environment.systemPackages = [ pkgs.docker-compose ];
 }
