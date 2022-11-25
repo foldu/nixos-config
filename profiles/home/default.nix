1e8184e7d0d268921eb0d7fb521dc4de2bcbc6e3
foldu <foldu@protonmail.com>
2022-11-25 12:02:25 +0100

    chore: Remove never used mounts

--- profiles/home/default.nix
+++ profiles/home/default.nix
@@ -45,32 +45,4 @@ in
   users.users.barnabas = {
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQ+TsvKvdWG+9KLVeg5N4y1Ce1jr/fP3ELTHVWLxZOR" ];
   };
-
-  fileSystems = with lib; flip mapAttrs' home-network.devices (
-    hostName: device:
-      let
-        myUser = config.users.users.barnabas;
-        uid = toString myUser.uid;
-      in
-      {
-        name = "/run/media/home/${hostName}";
-        value = {
-          # can't set device to ${myUser.home} because of infinite recursion
-          # TODO: investigate
-          device = "barnabas@${device.ip}:/home/barnabas";
-          fsType = "fuse.sshfs";
-          options = [
-            "IdentityFile=${myUser.home}/.ssh/home"
-            "IdentityAgent=/run/user/${uid}/ssh-agent"
-            "noauto"
-            "_netdev"
-            "x-systemd.automount"
-            "x-systemd.device-timeout=10"
-            "idmap=user"
-            "user"
-            "uid=${uid}"
-          ];
-        };
-      }
-  );
 }
