fa5c0127d58414bc0fb770598f1800696148019c
foldu <foldu@protonmail.com>
2022-12-25 13:35:27 +0100

    Change transmission download dir
    The old 1TB Hitachi HDD sounds more like a Hitachi excavator

--- profiles/homeserver/transmission.nix
+++ profiles/homeserver/transmission.nix
@@ -1,15 +1,27 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  torrentDir = "/srv/media/cia/data/torrents";
+  incompleteDir = "${torrentDir}/.incomplete";
+in
+{
   services.transmission = {
     enable = true;
     settings = {
-      download-dir = "/srv/media/aux/downloads";
-      incomplete-dir = "/srv/media/aux/downloads/.incomplete";
+      download-dir = torrentDir;
+      incomplete-dir = incompleteDir;
       incomplete-dir-enabled = true;
       rpc-host-whitelist = "torrent.home.5kw.li";
       rpc-whitelist = "127.0.0.1,192.168.1.*,192.168.100.*,10.20.30.*";
       rpc-port = 9091;
     };
   };
+
+  systemd.tmpfiles.rules = [
+    "d ${torrentDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
+    "d ${incompleteDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
+    "z ${torrentDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
+    "z ${incompleteDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
+  ];
 
   services.caddy.extraConfig = ''
     torrent.home.5kw.li {
