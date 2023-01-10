1a5d113e82b352b042cabe115b654001033e09a3
foldu <foldu@protonmail.com>
2023-01-10 14:31:28 +0100

    Cause underflow, use-after-free AND memory leaks in kernel bt

--- profiles/bluetooth.nix
+++ profiles/bluetooth.nix
@@ -1,14 +1,14 @@
 { config, lib, pkgs, ... }: {
   hardware.bluetooth = {
     enable = true;
-    package = pkgs.bluez.overrideAttrs (oldAttrs: {
-      dontStrip = true;
-      NIX_CFLAGS_COMPILE = "-ggdb -Og";
-    });
+    # package = pkgs.bluez.overrideAttrs (oldAttrs: {
+    #   dontStrip = true;
+    #   NIX_CFLAGS_COMPILE = "-ggdb -Og";
+    # });
     powerOnBoot = false;
   };
 
-  systemd.services.bluetooth-mesh.aliases = [ "dbus-org.bluez.mesh.service" ];
-  systemd.services.bluetooth.serviceConfig.ExecStart = [ "" "${pkgs.bluez}/libexec/bluetooth/bluetoothd -f /etc/bluetooth/main.conf -d" ];
-  systemd.services.bluetooth-mesh.serviceConfig.ExecStart = [ "" "${pkgs.bluez}/libexec/bluetooth/bluetooth-meshd -d" ];
+  # systemd.services.bluetooth-mesh.aliases = [ "dbus-org.bluez.mesh.service" ];
+  systemd.services.bluetooth.serviceConfig.ExecStart = [ "" "${pkgs.bluez}/libexec/bluetooth/bluetoothd -f /etc/bluetooth/main.conf" ];
+  systemd.services.bluetooth-mesh.serviceConfig.ExecStart = [ "" "${pkgs.bluez}/libexec/bluetooth/bluetooth-meshd -d --io=hci0" ];
 }
