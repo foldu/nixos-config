627f3f27c01b26ea9ac55fc52565e2ae36ddc5a5
foldu <foldu@protonmail.com>
2023-05-07 20:28:58 +0200

    Disable never activating NetworkManager-wait-online.service

--- nixos/common/profiles/desktop.nix
+++ nixos/common/profiles/desktop.nix
@@ -1,8 +1,11 @@
-{ ... }: {
+{ lib, ... }: {
   imports = [
     ../generic
     ../systemd-resolved.nix
   ];
+
+  # https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
 
   networking.networkmanager = {
     enable = true;
