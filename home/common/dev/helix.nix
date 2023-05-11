8844cd44e984bba3c4736f81723112ff888e92b4
foldu <foldu@protonmail.com>
2023-05-11 21:04:59 +0200

    Helix config

--- home/common/dev/helix.nix
+++ home/common/dev/helix.nix
@@ -3,10 +3,32 @@
     enable = true;
     settings = {
       theme = "kanagawa";
+      editor = {
+        # faster autocompletions in 100ms, default is 400ms
+        idle-timeout = 100;
+        statusline = {
+          # this is the default
+          # left = [ "mode" "spinner" "filename" "file-modification-indicator" ];
+          right = [ "diagnostics" "selections" "position" "position-percentage" ];
+          mode = {
+            normal = "NORMAL";
+            insert = "INSERT";
+            select = "SELECT";
+          };
+        };
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        lsp.display-messages = true;
+        soft-wrap.enable = true;
+      };
       keys = {
         normal = {
           space = {
             F = "file_picker_in_current_buffer_directory";
+            H = ":toggle lsp.display-inlay-hints";
           };
         };
       };
@@ -17,9 +39,23 @@
         auto-format = true;
         formatter = {
           command = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
-          args = [ ];
+        };
+      }
+      {
+        name = "rust";
+        language-server = {
+          command = "rustup";
+          args = [ "run" "nightly" "rust-analyzer" ];
         };
       }
     ];
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings.gui = {
+      showBottomBar = false;
+      showIcons = true;
+    };
   };
 }
