e182195b4d51f07f5446d9f88b6066a1f0711c59
foldu <foldu@protonmail.com>
2023-08-22 17:07:25 +0200

    Ignore btrfs file system usage

--- nixos/saturn/metrics/alerts/generic.nix
+++ nixos/saturn/metrics/alerts/generic.nix
@@ -43,7 +43,8 @@
   # };
 
   filesystem_full_80percent = {
-    condition = ''disk_used_percent{mode!="ro"} >= 80'';
+    # btrfs file system usage is wrong, so ignore it
+    condition = ''disk_used_percent{mode!="ro",fstype!="btrfs"} >= 80'';
     time = "10m";
     description = "{{$labels.instance}} device {{$labels.device}} on {{$labels.path}} got less than 20% space left on its filesystem";
   };
