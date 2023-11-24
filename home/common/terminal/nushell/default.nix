135457ecfa055cd7da7c995823c52faf75ca9a44
foldu <foldu@protonmail.com>
2023-11-24 12:46:59 +0100

    nushell: Fix deprecated dev-env

--- home/common/terminal/nushell/default.nix
+++ home/common/terminal/nushell/default.nix
@@ -11,7 +11,7 @@
     recursive = true;
   };
 
-  xdg.configFile."nushell/br.nu".source = pkgs.runCommandLocal "br.nu" { } ''
-    ${pkgs.broot}/bin/broot --print-shell-function nushell > $out
-  '';
+  # xdg.configFile."nushell/br.nu".source = pkgs.runCommandLocal "br.nu" { } ''
+  #   ${pkgs.broot}/bin/broot --print-shell-function nushell > $out
+  # '';
 }
