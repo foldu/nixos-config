df2569a1ccf7a223d5e59679920b2c0a4af38d66
foldu <foldu@protonmail.com>
2024-05-06 15:10:38 +0200

    Add bootstrap hosts file

--- nixos/common/dns-server.nix
+++ nixos/common/dns-server.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  bootstrapHosts = pkgs.writeTextFile {
+    name = "bootstraphosts.txt";
+    text = ''
+      100.64.0.3 git.home.5kw.li
+      100.64.0.3 ca.home.5kw.li
+      100.64.0.3 prometheus.home.5kw.li
+    '';
+  };
+in
 {
 
   networking.firewall.allowedUDPPorts = [ 53 ];
@@ -14,7 +24,10 @@
         "tcp-tls:1.0.0.1:853"
       ];
       hostsFile = {
-        sources = [ "https://git.home.5kw.li/foldu/hosts/raw/branch/master/tailscale.txt" ];
+        sources = [
+          "${bootstrapHosts}"
+          "https://git.home.5kw.li/foldu/hosts/raw/branch/master/tailscale.txt"
+        ];
         refreshPeriod = "5m";
       };
       blocking = {
@@ -30,14 +43,14 @@
             "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt"
             "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt"
             "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/AmazonFireTV.txt"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/alexa"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/apple"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/huawei"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/roku"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/samsung"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/sonos"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/windows"
-            "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/xiaomi"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/alexa"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/apple"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/huawei"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/roku"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/samsung"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/sonos"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/windows"
+            "https://raw.githubusercontent.com/nextdns/native-tracking-domains/master/domains/xiaomi"
             "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
             "https://raw.githubusercontent.com/nickspaargaren/pihole-google/master/categories/analyticsparsed"
             "https://raw.githubusercontent.com/nickspaargaren/pihole-google/master/categories/doubleclickparsed"
