2f107ff9938857930b2634ac371ff3a6198a26ac
foldu <foldu@protonmail.com>
2024-05-23 16:04:12 +0200

    Firefox: Disable scrollswitching

--- home/common/graphical/librewolf.nix
+++ home/common/graphical/librewolf.nix
@@ -9,6 +9,7 @@
       "network.cookie.lifetimePolicy" = 0;
       "identity.sync.tokenserver.uri" = "https://ffsync.home.5kw.li/1.0/sync/1.5";
       "identity.fxaccounts.enabled" = true;
+      "toolkit.tabbox.switchByScrolling" = false;
     };
   };
 }
