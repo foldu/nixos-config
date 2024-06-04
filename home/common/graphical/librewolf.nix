b8b1fefeb550162f1dcb448684a43b9f4bcc61d6
foldu <foldu@protonmail.com>
2024-06-04 08:45:28 +0200

    librewolf: Enable hardware video decoding

--- home/common/graphical/librewolf.nix
+++ home/common/graphical/librewolf.nix
@@ -9,6 +9,10 @@
       "identity.sync.tokenserver.uri" = "https://ffsync.home.5kw.li/1.0/sync/1.5";
       "identity.fxaccounts.enabled" = true;
       "toolkit.tabbox.switchByScrolling" = false;
+      # enable hardware video decoding
+      "media.ffmpeg.vaapi.enabled" = true;
+      # use desktop portal filepicker
+      "widget.use-xdg-desktop-portal.file-picker" = 1;
     };
   };
 }
