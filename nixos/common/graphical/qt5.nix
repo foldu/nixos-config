012ed320b1cbfc1aa83bcf306c766f74e040aa54
foldu <foldu@protonmail.com>
2024-10-27 22:05:09 +0100

    Make qt less uglier

--- nixos/common/graphical/qt5.nix
+++ nixos/common/graphical/qt5.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  qt = {
+    enable = true;
+    style = "adwaita-dark";
+  };
   #programs.qt5ct.enable = true;
   # environment.sessionVariables."QT_QPA_PLATFORM" = "xcb";
   #environment.systemPackages = [
