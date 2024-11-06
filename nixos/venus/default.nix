3a19d551b2b4b196c1d099aab87343a05ec9e9e2
foldu <foldu@protonmail.com>
2024-11-06 14:38:05 +0100

    Use newer kernel on venus

--- nixos/venus/default.nix
+++ nixos/venus/default.nix
@@ -45,8 +45,6 @@
 
   services.fwupd.enable = true;
 
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_10;
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
