26c6d28a82b32864120f3789020a71cf404af28b
foldu <foldu@protonmail.com>
2024-11-17 11:13:20 +0100

    More fisk customization

--- home/common/terminal/fish/default.nix
+++ home/common/terminal/fish/default.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
-
   programs.kitty.shellIntegration.mode = "no-cursor";
+
+  programs.nix-index.enable = true;
 
   programs.fish = {
     enable = true;
@@ -10,8 +11,26 @@
       mv = "mv -iv";
       cp = "cp -iv";
       rsync = "rsync -azvP";
+      sm = "src-manage";
     };
-    plugins = [ ];
+    interactiveShellInit = ''
+      set -g tide_character_icon '$'
+      set -g fish_greeting
+    '';
+    shellAliases = {
+      ls = "ls --hyperlink=auto --color=auto";
+    };
+    plugins = [
+      {
+        name = "tide";
+        src = pkgs.fetchFromGitHub {
+          owner = "IlanCosman";
+          repo = "tide";
+          rev = "44c521ab292f0eb659a9e2e1b6f83f5f0595fcbd";
+          sha256 = "sha256-85iU1QzcZmZYGhK30/ZaKwJNLTsx+j3w6St8bFiQWxc=";
+        };
+      }
+    ];
   };
 
   programs.zoxide = {
