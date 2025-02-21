d5339cd3ccfba5902c03fafd49daed40a8f3d152
foldu <foldu@protonmail.com>
2025-02-21 20:21:06 +0100

    Add user/pass auth to bad transmission script

--- nixos/saturn/transmission/start-stop-torrents/main.go
+++ nixos/saturn/transmission/start-stop-torrents/main.go
@@ -12,12 +12,15 @@ import (
 func main() {
 	uri := expectEnv("TRANSMISSION_URL")
 	action := expectEnv("ACTION")
+	user := expectEnv("USER")
+	pass := expectEnv("PASS")
 
 	endpoint, err := url.Parse(uri)
 	if err != nil {
 		log.Fatalf("Invalid url %v: %v", uri, err)
 	}
 
+	endpoint.User = url.UserPassword(user, pass)
 	client, err := transmissionrpc.New(endpoint, nil)
 	if err != nil {
 		log.Fatalf("Failed connecting to %v: %v", endpoint, err)
