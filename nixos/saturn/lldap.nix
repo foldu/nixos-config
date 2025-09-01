dc96a2b2c06764f900ce9dd3f3b3d1c470d4c36c
foldu <foldu@protonmail.com>
2025-09-01 21:05:50 +0200

    Fix lldap config

--- nixos/saturn/lldap.nix
+++ nixos/saturn/lldap.nix
@@ -7,13 +7,13 @@ in
     enable = true;
     environment = {
       LLDAP_JWT_SECRET_FILE = "/var/secrets/lldap/jwt_secret";
-      LLDAP_LDAP_USER_PASS_FILE = "/var/secrets/lldap/admin_pass";
     };
     settings = {
       ldap_base_dn = "dc=5kw,dc=li";
       ldap_user_email = "admin@home.5kw.li";
       http_url = "https://${domain}";
       database_url = "postgresql:///lldap?host=/run/postgresql";
+      ldap_user_pass_file = "/var/secrets/lldap/admin_pass";
     };
   };
 
