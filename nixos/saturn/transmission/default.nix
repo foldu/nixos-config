eee8ea3bd40483b7590905e1baca5ae70f2539c6
foldu <foldu@protonmail.com>
2025-09-01 11:58:33 +0200

    Clean up transmission config

--- nixos/saturn/transmission/default.nix
+++ nixos/saturn/transmission/default.nix
@@ -1,22 +1,13 @@
 {
   config,
-  lib,
-  pkgs,
-  home-network,
   ...
 }:
 let
   torrentDir = "/srv/media/blub/data/torrents";
   configDir = "/var/lib/transmission/config";
-  incompleteDir = "${torrentDir}/.incomplete";
   watchDir = "${torrentDir}/watch";
   rpcPort = "9091";
   domain = "torrent.home.5kw.li";
-  start-stop-torrents = pkgs.buildGoModule {
-    name = "start-stop-torrents";
-    src = ./start-stop-torrents;
-    vendorHash = "sha256-jnSdAsXlzQBpZer5eCdFjWp3CR8wmOd8Nv2XNdPvlXo=";
-  };
 in
 {
   users.users.transmission = {
@@ -67,73 +58,15 @@ in
       };
     };
 
-  # virtualisation.quadlet.containers.transmission.containerConfig = {
-  #   image = "docker.io/haugene/transmission-openvpn";
-  #   environments = {
-  #     PUID = toString config.users.users.transmission.uid;
-  #     PGID = toString config.users.groups.transmission.gid;
-  #     TRANSMISSION_RPC_USERNAME = "barnabas";
-  #     TRANSMISSION_DOWNLOAD_DIR = "/data";
-  #     TRANSMISSION_INCOMPLETE_DIR = "/data/.incomplete";
-  #     TRANSMISSION_RPC_PORT = rpcPort;
-  #   };
-  #   publishPorts = [ "${rpcPort}:${rpcPort}" ];
-  #   volumes = [
-  #     "${configDir}:/config"
-  #     "${torrentDir}:/data"
-  #   ];
-  #   environmentFiles = [ "/var/secrets/transmission.env" ];
-  #   addCapabilities = [ "NET_ADMIN" ];
-  #   podmanArgs = [ "--privileged" ];
-  # };
-
   users.users.barnabas.extraGroups = [ "transmission" ];
 
   systemd.tmpfiles.rules = [
-    "d ${torrentDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
-    "d ${incompleteDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
-    "z ${torrentDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
-    "z ${incompleteDir} 755 ${config.services.transmission.user} ${config.services.transmission.group}"
+    "d ${torrentDir} 775 ${config.services.transmission.user} ${config.services.transmission.group}"
+    "d ${watchDir} 775 ${config.services.transmission.user} ${config.services.transmission.group}"
+    "Z ${torrentDir} 775 ${config.services.transmission.user} ${config.services.transmission.group}"
     "d ${configDir} 700 transmission transmission"
+    "Z ${configDir} 700 transmission transmission"
   ];
-
-  systemd.services =
-    let
-      defaultArgs = {
-        serviceConfig = {
-          ExecStart = "${start-stop-torrents}/bin/start-stop-torrents";
-          EnvironmentFile = "/var/secrets/transmission.env";
-        };
-        environment = {
-          TRANSMISSION_URL = "https://${domain}/transmission/rpc";
-        };
-      };
-    in
-    {
-      transmission-start-all = lib.attrsets.recursiveUpdate defaultArgs {
-        environment.ACTION = "start-all";
-      };
-      transmission-stop-all = lib.attrsets.recursiveUpdate defaultArgs {
-        environment.ACTION = "stop-all";
-      };
-    };
-
-  systemd.timers = {
-    transmission-start-all = {
-      enable = true;
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnCalendar = "*-*-* 02:00:00";
-      };
-    };
-    transmission-stop-all = {
-      enable = true;
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnCalendar = "*-*-* 09:00:00";
-      };
-    };
-  };
 
   services.caddy.extraConfig = ''
     ${domain} {
--- nixos/saturn/transmission/start-stop-torrents/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/foldu/start-stop-torrents
-
-go 1.22.3
-
-require (
-	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hekmon/cunits/v2 v2.1.0 // indirect
-	github.com/hekmon/transmissionrpc/v3 v3.0.0 // direct
-)
