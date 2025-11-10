6917ee4671d23ca49a969857ade189e830b1d092
foldu <foldu@protonmail.com>
2025-11-10 13:46:35 +0100

    Enable SSO for open webui

--- nixos/saturn/open-webui.nix
+++ nixos/saturn/open-webui.nix
@@ -1,6 +1,5 @@
 { inputs, ... }:
 let
-  openwebui-port = 7742;
   openwebui-data = "/var/lib/openwebui";
 in
 {
@@ -12,25 +11,38 @@ in
     "d ${openwebui-data} 700 root root"
   ];
 
+  virtualisation.quadlet.autoEscape = true;
+
   virtualisation.quadlet.containers.open-webui = {
     containerConfig = {
       image = "ghcr.io/open-webui/open-webui:main";
       volumes = [
         "${openwebui-data}:/app/backend/data"
       ];
-      publishPorts = [
-        "${toString openwebui-port}:8080"
-      ];
       autoUpdate = "registry";
+      environmentFiles = [ "/var/secrets/openwebui.env" ];
       environments = {
         WEBUI_URL = "https://ai.home.5kw.li";
+        ENABLE_OAUTH_SIGNUP = "true";
+        OAUTH_MERGE_ACCOUNTS_BY_EMAIL = "true";
+        OAUTH_CLIENT_ID = "HNcsNHmq~dNWJ-CPSPKzV-G4ifjsgTcTYhLcIpLE4lTCae_3YxXAOzGDN2BPWi-E";
+        OPENID_PROVIDER_URL = "https://auth.home.5kw.li/.well-known/openid-configuration";
+        OAUTH_PROVIDER_NAME = "Authelia";
+        OAUTH_SCOPES = "openid email profile groups";
+        ENABLE_OAUTH_ROLE_MANAGEMENT = "true";
+        OAUTH_ALLOWED_ROLES = "openwebui,openwebui-admin";
+        OAUTH_ADMIN_ROLES = "openwebui-admin";
+        OAUTH_ROLES_CLAIM = "groups";
+        # OPENID_REDIRECT_URI = "https://ai.home.5kw.li/oauth/oidc/callback";
       };
+      # needed because oidc is on tailscale
+      networks = [ "host" ];
     };
   };
 
   services.caddy.extraConfig = ''
     ai.home.5kw.li {
-      reverse_proxy localhost:${toString openwebui-port}
+      reverse_proxy localhost:${toString 8080}
     }
   '';
 }
