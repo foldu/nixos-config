bc59a84276682024483b479940cbccf21f29067f
foldu <foldu@protonmail.com>
2025-12-13 12:40:59 +0100

    Update redlib

--- nixos/saturn/redlib/package.nix
+++ nixos/saturn/redlib/package.nix
@@ -1,72 +1,83 @@
 {
   lib,
-  stdenv,
   cacert,
   nixosTests,
   rustPlatform,
   fetchFromGitHub,
-  darwin,
+  nix-update-script,
 }:
-rustPlatform.buildRustPackage rec {
+
+rustPlatform.buildRustPackage {
   pname = "redlib";
   version = "unstable";
 
   src = fetchFromGitHub {
     owner = "redlib-org";
     repo = "redlib";
-    rev = "dcb507d56710a047c743fe79a5326e4f1ca930a6";
-    hash = "sha256-ANNZEDLbiZr3i2FmeyAft8bkKh2xz12dYD8dXEbWe48=";
+    rev = "9e1c09610cdcb073d16559713dee6409d1a08b20";
+    hash = "sha256-ERTEoT7w8oGA0ztrzc9r9Bl/7OOay+APg3pW+h3tgvM=";
   };
 
-  useFetchCargoVendor = true;
+  cargoHash = "sha256-ageSjIX0BLVYlLAjeojQq5N6/VASOIpwXNR/3msl/p4=";
 
-  cargoHash = "sha256-FDeENHY6bwwCq6leSoIuCqPI6PCHpEod7KN2grS2gFw=";
-
-  buildInputs = lib.optionals stdenv.isDarwin [
-    darwin.apple_sdk.frameworks.Security
-  ];
-
-  doCheck = false;
+  postInstall = ''
+    install --mode=444 -D contrib/redlib.service $out/lib/systemd/system/redlib.service
+    substituteInPlace $out/lib/systemd/system/redlib.service \
+      --replace-fail "/usr/bin/redlib" "$out/bin/redlib"
+  '';
 
   checkFlags = [
     # All these test try to connect to Reddit.
+    # utils.rs
     "--skip=test_fetching_subreddit_quarantined"
     "--skip=test_fetching_nsfw_subreddit"
     "--skip=test_fetching_ws"
 
+    # client.rs
     "--skip=test_obfuscated_share_link"
     "--skip=test_share_link_strip_json"
-
     "--skip=test_localization_popular"
-    "--skip=test_fetching_subreddit"
-    "--skip=test_fetching_user"
+    "--skip=test_private_sub"
+    "--skip=test_banned_sub"
+    "--skip=test_gated_sub"
+    "--skip=test_default_subscriptions"
+    "--skip=test_rate_limit_check"
 
+    # subreddit.rs
+    "--skip=test_fetching_subreddit"
     "--skip=test_gated_and_quarantined"
 
-    "--skip=test_compress_response"
-    "--skip=test_private_sub"
+    # user.rs
+    "--skip=test_fetching_user"
 
     # These try to connect to the oauth client
+    # oauth.rs
     "--skip=test_oauth_client"
     "--skip=test_oauth_client_refresh"
     "--skip=test_oauth_token_exists"
     "--skip=test_oauth_headers_len"
+
+    "--skip=oauth::test_generic_web_backend"
+    "--skip=oauth::test_mobile_spoof_backend"
   ];
 
   env = {
     SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
   };
 
-  passthru.tests = {
-    inherit (nixosTests) redlib;
+  passthru = {
+    tests = nixosTests.redlib;
+    updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
   };
 
   meta = {
-    changelog = "https://github.com/redlib-org/redlib/releases/tag/v${version}";
     description = "Private front-end for Reddit (Continued fork of Libreddit)";
     homepage = "https://github.com/redlib-org/redlib";
     license = lib.licenses.agpl3Only;
     mainProgram = "redlib";
-    maintainers = with lib.maintainers; [ soispha ];
+    maintainers = with lib.maintainers; [
+      bpeetz
+      Guanran928
+    ];
   };
 }
