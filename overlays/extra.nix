5652b3d73e33c8c6673666e688eae67a84c3ef5f
foldu <foldu@protonmail.com>
2026-01-16 12:38:59 +0100

    Remove unused files

--- overlays/extra.nix
+++ overlays/extra.nix
@@ -1,7 +1,3 @@
 final: prev: {
-  gnomeExtensions = prev.gnomeExtensions // {
-    gnome-ui-tune = prev.callPackage ../packages/gnome-ui-tune { };
-  };
   domitian = prev.callPackage ../packages/domitian { };
-  amdgpu-module = prev.callPackage ../packages/amdgpu-module { };
 }
