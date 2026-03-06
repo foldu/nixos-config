e258d146fd397146e69e22892f2234b0ad10a317
foldu <foldu@protonmail.com>
2026-03-06 08:49:40 +0100

    Get invidious to work again

--- nixos/saturn/invidious.nix
+++ nixos/saturn/invidious.nix
@@ -1,9 +1,9 @@
 { config, ... }:
 let
   domain = "invidious.home.5kw.li";
+  companionPort = "8282";
 in
 {
-
   services.invidious = {
     enable = true;
     inherit domain;
@@ -16,8 +16,43 @@ in
       https_only = true;
       external_port = 443;
       address = "127.0.0.1";
+      invidious_companion = [
+        {
+          private_url = "http://localhost:${companionPort}/companion";
+        }
+      ];
+      invidious_companion_key = "aephahm5Ca2ut6ha";
     };
   };
+
+  virtualisation.quadlet =
+    let
+      inherit (config.virtualisation.quadlet) volumes;
+    in
+    {
+      containers = {
+        invidious-companion = {
+          containerConfig = {
+            environments = {
+              SERVER_SECRET_KEY = "aephahm5Ca2ut6ha";
+              PORT = "8282";
+            };
+            image = "quay.io/invidious/invidious-companion:latest";
+            volumes = [
+              "${volumes.invidious-companion-cache.ref}:/var/tmp/youtubei.js:rw"
+            ];
+            publishPorts = [
+              "127.0.0.1:${companionPort}:${companionPort}"
+            ];
+            autoUpdate = "registry";
+            noNewPrivileges = true;
+            dropCapabilities = [ "ALL" ];
+            readOnly = true;
+          };
+        };
+      };
+      volumes.invidious-companion-cache.volumeConfig = { };
+    };
 
   services.caddy.extraConfig = ''
     ${domain} {
