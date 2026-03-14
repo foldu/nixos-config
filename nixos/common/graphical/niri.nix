33986a81f435ec8d22f7b0a038a8ac7794ffe902
foldu <foldu@protonmail.com>
2026-03-14 17:22:56 +0100

    This probably has a reason

--- nixos/common/graphical/niri.nix
+++ nixos/common/graphical/niri.nix
@@ -12,7 +12,7 @@
   };
   xdg.portal.config.niri = {
     "org.freedesktop.impl.portal.FileChooser" = "kde";
-    "org.freedesktop.impl.portal.Secret" = lib.mkForce "kwallet";
+    # "org.freedesktop.impl.portal.Secret" = lib.mkForce "kwallet";
   };
 
   # needed for noctalia
