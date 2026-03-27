4228b522b03c79463376854d4e0da3a635a4bf0b
foldu <foldu@protonmail.com>
2026-03-27 08:15:46 +0100

    Modernize syncthing settings and don't connect over tailscale
    Will cause tailscale to spam the logs because other thing is down

--- nixos/common/graphical/syncthing.nix
+++ nixos/common/graphical/syncthing.nix
@@ -1,55 +1,39 @@
 {
   config,
   lib,
-  home-network,
   ...
 }:
-let
-  syncthingPort = 22000;
-  devices = home-network.devices;
-in
 {
-  networking.firewall = {
-    allowedTCPPorts = [
-      # syncthing file transfer
-      syncthingPort
-    ];
-    allowedUDPPorts = [
-      # syncthing discovery
-      21027
-    ];
-  };
-
   services.syncthing =
     let
       syncthingDevices = {
         jupiter = {
           id = "O7RPI7X-O7EEEJO-TH55KF5-64PE6MS-RPFJZ5B-LIA2ZEW-GJVBCHS-76W54AP";
-          addresses = [ "tcp://${devices.jupiter.vip}:${toString syncthingPort}" ];
         };
         venus = {
           id = "7QLRH3I-32ELROX-SSYZXEI-BAZYYDW-AA5ASUJ-RVGY4EG-KDYT66K-XFMPVQW";
-          addresses = [ "tcp://${devices.venus.vip}:${toString syncthingPort}" ];
         };
       };
       otherDevices = lib.filterAttrs (k: _: k != config.networking.hostName) syncthingDevices;
       allOtherDeviceNames = lib.attrNames otherDevices;
-      mkSharedShare = id: {
-        inherit id;
+      mkSharedShare = path: {
+        inherit path;
         devices = allOtherDeviceNames;
+        ignorePerms = false;
       };
     in
     {
       enable = true;
       user = "barnabas";
       group = "users";
+      openDefaultPorts = true;
       dataDir = "/home/barnabas";
       settings = {
         devices = syncthingDevices;
         folders = {
-          "/home/barnabas/downloads" = mkSharedShare "downloads";
-          "/home/barnabas/uni" = mkSharedShare "uni";
-          "/home/barnabas/sync" = mkSharedShare "sync";
+          "downloads" = mkSharedShare "/home/barnabas/downloads";
+          "uni" = mkSharedShare "/home/barnabas/uni";
+          "sync" = mkSharedShare "/home/barnabas/sync";
         };
       };
     };
