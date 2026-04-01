a0b67d1e40a6702c6ef53d3369b3fe407e0faa8a
foldu <foldu@protonmail.com>
2026-04-01 19:14:29 +0200

    Remove unused file

--- home/common/graphical/niri/default.nix
+++ home/common/graphical/niri/default.nix
@@ -7,6 +7,7 @@
 
   xdg.configFile = {
     "niri/config.kdl".source = ./config.kdl;
+    # maybe just use a service for this? I don't think the tray stuff is up right after start, given how ass slow qt is
     "niri/launch-bitwarden.sh" = {
       executable = true;
       source = ./launch-bitwarden.sh;
