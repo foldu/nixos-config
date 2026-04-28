c1eb1a804b7193f64d71ce639ff1979fff1dc68f
foldu <foldu@protonmail.com>
2026-04-28 22:23:30 +0200

    Fix broken bluetooth audio

--- nixos/common/bluetooth.nix
+++ nixos/common/bluetooth.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    package = pkgs.bluez.overrideAttrs (old: {
+      patches = [
+        (pkgs.fetchpatch {
+          url = "https://github.com/bluez/bluez/commit/066a164a524e4983b850f5659b921cb42f84a0e0.patch";
+          sha256 = "sha256-I1WoBJZEZJ05hwGuksp52I4FLJ+jbG9t7U2sLTFmU0w=";
+        })
+      ];
+    });
+  };
 }
