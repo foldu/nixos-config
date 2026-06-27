71a8bb96d194bfcee4cc72bbf21fa2d21c4b575d
foldu <foldu@protonmail.com>
2026-06-27 11:45:19 +0200

    Update helium

--- packages/helium/default.nix
+++ packages/helium/default.nix
@@ -11,11 +11,11 @@
 appimageTools.wrapType2 rec {
 
   pname = "helium";
-  version = "0.13.4.1";
+  version = "0.13.6.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-    sha256 = "sha256-z23up+T6bj6F+cQslmI92bEksIAw1OQHRIrmQSaaxY8=";
+    sha256 = "sha256-ZcZo/vFXWrZjuPjIt2MYbsxs4LU7NvpB3I6mrPzAJjE=";
   };
 
   _enableFeatures =
