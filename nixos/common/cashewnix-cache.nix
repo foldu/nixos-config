96b2ecf2b7848c135c2c2e9bbc343dbc8b58931a
foldu <foldu@protonmail.com>
2026-07-11 16:20:52 +0200

    Upgrade to newer cashewnix

--- nixos/common/cashewnix-cache.nix
+++ nixos/common/cashewnix-cache.nix
@@ -1,13 +1,26 @@
 { config, ... }:
+let
+  harmoniaPort = 5000;
+in
 {
   imports = [ ./cashewnix.nix ];
 
   sops.secrets."cashewnix-private" = { };
 
   services.cashewnix = {
-    privateKeyPath = config.sops.secrets."cashewnix-private".path;
-    enableNixServe = true;
+    settings.local_binary_caches.local_cache = {
+      advertise = "ip";
+      port = harmoniaPort;
+    };
   };
 
-  services.nix-serve.openFirewall = true;
+  services.harmonia.cache = {
+    enable = true;
+    signKeyPaths = [ config.sops.secrets."cashewnix-private".path ];
+    settings = {
+      bind = "0.0.0.0:${toString harmoniaPort}";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ harmoniaPort ];
 }
