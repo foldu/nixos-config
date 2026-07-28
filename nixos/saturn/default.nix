565f12102b210c447f1d84f8834b82c8eb2e7a68
foldu <foldu@protonmail.com>
2026-07-28 19:10:21 +0200

    Update caddy hash

--- nixos/saturn/default.nix
+++ nixos/saturn/default.nix
@@ -64,7 +64,7 @@
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/ovh@v1.1.0" ];
       # don't forget to update this caddy hash/caddyhash
-      hash = "sha256-7m1sUhlI8i0NGUT2IPwbzdqAOYeUoB7CYVYMa+UWKMc=";
+      hash = "sha256-/xpTqYydmJEthBgGJ3uZ9FDF19dlvWs0h8XUf8KkS/M=";
     };
     globalConfig = ''
       acme_dns ovh {
