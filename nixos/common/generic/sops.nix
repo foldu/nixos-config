5f7e4316ee0e33dda61a23efb471d86d5066c730
foldu <foldu@protonmail.com>
2026-08-03 19:56:26 +0200

    sops: per-host ssh host keys as identities on all members
    All hosts previously decrypted with the same admin key copied to
    /var/secrets/sops_keys.txt: one shared secret fleet-wide, provisioned
    out-of-band
    
    Why: no out-of-band bootstrap (host keys exist at activation),
    compromised saturn now has the possibility to leak absolutely
    everything.

--- nixos/common/generic/sops.nix
+++ nixos/common/generic/sops.nix
@@ -5,10 +5,14 @@
   environment.systemPackages = with pkgs; [
     age
     sops
+    ssh-to-age
   ];
 
   sops.defaultSopsFile = ../../../secrets/secrets.yaml;
-  sops.age.keyFile = "/var/secrets/sops_keys.txt";
 
+  # Per-host identity = the ssh host key (see the server_* recipients in
+  # .sops.yaml). The admin key (admin_barnabas) is the backed-up recovery,
+  # if the host key breaks, use it to add the new one in .sops.yaml and `sops updatekeys`
+  # admin key is in bitwarden vault
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 }
