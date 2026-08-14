d0acd2ca6af0076c6acedeee2c10232e2d9e4820
foldu <foldu@protonmail.com>
2026-08-14 15:23:21 +0200

    helium: 0.15.3.1 -> 0.15.4.1
    Diff: https://github.com/imputnet/helium-linux/compare/0.15.3.1...0.15.4.1

--- packages/helium/default.nix
+++ packages/helium/default.nix
@@ -11,11 +11,11 @@
 appimageTools.wrapType2 rec {
 
   pname = "helium";
-  version = "0.15.3.1";
+  version = "0.15.4.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-    sha256 = "sha256-ZCCm/prkgYgbDHW6OBPWvoIE77g7IYQpYdqc/PnIrSU=";
+    sha256 = "sha256-h3yxZnMb/EHvPJALQlJgHUVYUNsfuv0pnewgf6K6sx8=";
   };
 
   _enableFeatures =
