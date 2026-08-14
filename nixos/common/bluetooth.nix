2bc7fadcfe1cb539b4057e0afc90eecf540000b9
foldu <foldu@protonmail.com>
2026-08-14 18:46:01 +0200

    bluetooth: drop patch as it already was in newest bluez

--- nixos/common/bluetooth.nix
+++ nixos/common/bluetooth.nix
@@ -1,14 +1,4 @@
 { pkgs, ... }:
 {
-  hardware.bluetooth = {
-    enable = true;
-    package = pkgs.bluez.overrideAttrs (old: {
-      patches = [
-        (pkgs.fetchpatch {
-          url = "https://github.com/bluez/bluez/commit/066a164a524e4983b850f5659b921cb42f84a0e0.patch";
-          sha256 = "sha256-I1WoBJZEZJ05hwGuksp52I4FLJ+jbG9t7U2sLTFmU0w=";
-        })
-      ];
-    });
-  };
+  hardware.bluetooth.enable = true;
 }
