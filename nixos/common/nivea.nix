45157b8c361dc08517b86706309e3068911cd25d
foldu <foldu@protonmail.com>
2026-08-28 20:19:46 +0200

    saturn: Do not install useless nvidia settings program that doesn't even compile

--- nixos/common/nivea.nix
+++ nixos/common/nivea.nix
@@ -3,6 +3,8 @@
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;
+  # don't install nvidia-settings (GUI tool, useless on a headless server)
+  hardware.nvidia.nvidiaSettings = false;
   hardware.nvidia-container-toolkit.enable = true;
 
   services.telegraf.extraConfig.inputs.nvidia_smi = {
